51069d4d25cec4a9f8676141ce5be47c5778e19c
Nicholas Leaman <nicholasleaman@Nicholass-MacBook-Pro.local>
2019-11-18 13:03:45 +0100

    changed something

--- 100excercise.py
+++ 100excercise.py
@@ -1,7 +1,8 @@
 #create dictionary of keys a,b,c. each key has as value a ist from 1 - 10, 11 - 20, anmd 21 -30
 
 test = dict(a = list(1,2,3,4,5,6,7,8,9,10), b = list(11,12,13,14,15,16,17,18,19,20), c = list(21,22,23,24,25,26,27,28,29,30))
-print(test)
+
+print("THIS IS A TEST")
 
 
 
